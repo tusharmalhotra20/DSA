3af754655ff6e85bb2da04e31596c4cf9a4cd625
tusharmalhotra20 <tusharmalhotra494@gmail.com>
2023-01-24 23:56:03 +0530

    DP started

---
+++ Cpp_Training/Level_1/8_DP/p001.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int fibonacci(int n)
+{
+    if (n <= 1)
+        return n;
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main()
+{
+
+    int num{0};
+    cin >> num;
+
+    cout << fibonacci(num);
+    return 0;
+}
